71766e188f31eebaeb2261be5a22c9ea16bf4f66
yasmintuhfah <95863438+yasmintuhfah@users.noreply.github.com>
2022-02-13 22:26:15 +0700

    Add files via upload

--- Operators/src/FloatingPoint.java
+++ Operators/src/FloatingPoint.java
@@ -1,1 +1,15 @@
-
+package Operators;
+/** <H1> Floating Point </h1>
+ * <p><q> Program ini dibuat untuk menggunakan class Math (round) </q></p>
+ * 
+ * @author YasminA
+ * @version 1.0
+ * @since 2022-02-13
+*/
+public class FloatingPoint {
+	public static void main(String[] args) {
+		double x = 92.98;
+		int nx = (int) Math.round(x);
+		System.out.print(nx);
+	}
+}
