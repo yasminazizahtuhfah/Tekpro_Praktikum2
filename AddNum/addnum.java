87786001110d5cbac744e7177b6902e70e2e2fef
yasmintuhfah <95863438+yasmintuhfah@users.noreply.github.com>
2022-02-13 19:30:32 +0700

    Add files via upload

--- AddNum/addnum.java
+++ AddNum/addnum.java
@@ -1,1 +1,43 @@
+package AddNum;
+import java.io.*;
 
+/**
+* <h1>Add Two Numbers!</h1>
+* The AddNum program implements an application that
+* simply adds two given integer numbers and Prints
+* the output on the screen.
+* <p>
+* <b>Note:</b> Giving proper comments in your program makes it more
+* user friendly and it is assumed as a high quality code.
+*
+* @author YasminA
+* @version 1.0
+* @since 2022-02-10
+*/
+
+public class addnum {
+	/**
+	* This method is used to add two integers. This is
+	* a the simplest form of a class method, just to
+	* show the usage of various javadoc Tags.
+	* @param numA This is the first paramter to addNum method
+	* @param numB This is the second parameter to addNum method
+	* @return int This returns sum of numA and numB.
+	*/
+	public int addNum(int numA, int numB) {
+	return numA + numB;
+	}
+
+/**
+* This is the main method which makes use of addNum method.
+* @param args Unused.
+* @exception IOException On input error.
+* @see IOException
+*/
+
+public static void main(String args[]) throws IOException {
+addnum obj = new addnum();
+int sum = obj.addNum(10, 20);
+System.out.println("Sum of 10 and 20 is :" + sum);
+}
+}
